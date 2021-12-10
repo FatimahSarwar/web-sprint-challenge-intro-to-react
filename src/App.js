ff67db10ee0df964b2acaad7b8349f154a2b7ca7
Fatimah Sarwar <fatimahsarwar@Fatimahs-MBP.fios-router.home>
2021-12-10 12:48:33 -0500

    Done with adding data. Now going to style.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React , {useState , useEffect} from 'react';
 import './App.css';
-
+import axios from 'axios';
+import StarCharacter from './components/Character'
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+const [character , setCharacter] = useState([]);
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+useEffect(()=>{
+  axios.get(`https://swapi.dev/api/people`)
+  .then(response =>{
+    console.log(response.data);
+    setCharacter(response.data)
+  })
+},[])
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">React Wars</h1>
+{
+  character.map((character,index)=>{
+return <StarCharacter key = {index} info = {character} />
+  })
+}
+  
+
     </div>
   );
 }
-
+// console.log(`https://swapi.dev/api/people`)
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,9 @@
 // Write your Character component here
+export default function StarCharacter(props) {
+const {info} = props;
+return (
+    <h2> {info.name}</h2>
+)
+
+}
+
